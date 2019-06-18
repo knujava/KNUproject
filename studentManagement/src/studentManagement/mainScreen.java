40fb1b79ef18007519b9a9beb10bb89f04213df7
jobgunwiz <50469718+jobgunwiz@users.noreply.github.com>
2019-06-18 17:40:55 +0900

    exit
    exit button doesn't work...

--- studentManagement/src/studentManagement/mainScreen.java
+++ studentManagement/src/studentManagement/mainScreen.java
@@ -12,24 +12,26 @@ public class mainScreen extends JFrame implements ActionListener{
 		gui.setVisible(true);
 	}
 
+	static String contents[][] = {
+			{"박", "김"},
+			{"이", "최"},
+			{"조", "권"}
+	};
+	
 	public mainScreen() {
 		super("WELCOME! :::: Professor");
 		setSize(500,700);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		String header[] = {"a","b"};
-		String contents[][] = {
-				{"박", "김"},
-				{"이", "최"},
-				{"조", "권"}
-		};
+		
 		
 		JTable table = new JTable(contents, header);
 		JScrollPane Scrollpane = new JScrollPane(table);
 		add(Scrollpane);
 		pack();//
 		
-		JButton actionButton = new JButton("hhhhhhhhhhhhhhhhhhhh");
+		JButton actionButton = new JButton("hid");
 		add(actionButton);
 	}
 	
@@ -37,5 +39,10 @@ public class mainScreen extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+		String buttonString = e.getActionCommand();
+		
+		if (buttonString.equals("hid")) {
+			System.exit(0);
+		}
 	}
 }
