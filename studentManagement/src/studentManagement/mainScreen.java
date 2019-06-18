f62c169566321a5feacc9c14f3449662a85d42bf
jobgunwiz <50469718+jobgunwiz@users.noreply.github.com>
2019-06-18 17:14:10 +0900

    table practice
    just...

--- studentManagement/src/studentManagement/mainScreen.java
+++ studentManagement/src/studentManagement/mainScreen.java
@@ -1,11 +1,39 @@
 package studentManagement;
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class mainScreen {
+public class mainScreen extends JFrame implements ActionListener{
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("help me");
+		mainScreen gui = new mainScreen();
+		gui.setVisible(true);
+	}
+
+	public mainScreen() {
+		super("WELCOME! :::: Professor");
+		setSize(500,700);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		String header[] = {"a","b"};
+		String contents[][] = {
+				{"박", "김"},
+				{"이", "최"},
+				{"조", "권"}
+		};
+		
+		JTable table = new JTable(contents, header);
+		JScrollPane Scrollpane = new JScrollPane(table);
+		add(Scrollpane);
+		pack();//
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
