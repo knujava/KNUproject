d0e88db5475d9c724ef764c41bc321acbad59c92
jobgunwiz <50469718+jobgunwiz@users.noreply.github.com>
2019-06-19 15:33:30 +0900

    tmpsave
    save

--- studentManagement/src/studentManagement/StudentGrade.java
+++ studentManagement/src/studentManagement/StudentGrade.java
@@ -1,18 +1,33 @@
 package studentManagement;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 public class StudentGrade extends JFrame implements ActionListener{
+	
+	
+	private JPanel tablePanel;
+	private JTextField name;
+	private JTextField grade;
+	private JTextField subj;
+	
+	
 	public StudentGrade() {
-		super("WELCOME! :::: student");
+		super("WELCOME! :::: PROFESSOR");
 		setSize(500,700);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
 		
 		String header[] = {"학번","이름","과목명","평점"};
 		String contents[][] = {
@@ -22,16 +37,54 @@ public class StudentGrade extends JFrame implements ActionListener{
 				{"2018000000", "박맹구", "팔공산", "1.7"},
 				{"2019000000", "이훈", "초급교양영어", "2.7"}
 		};
-		
+		tablePanel = new JPanel();
 		
 		JTable table = new JTable(contents, header);
 		JScrollPane Scrollpane = new JScrollPane(table);
-		add(Scrollpane);
-		pack();//
+		add(Scrollpane, BorderLayout.NORTH);
+		//pack();//
 		
-		JButton actionButton = new JButton("hid");
-		actionButton.addActionListener(this);//
-		add(actionButton);
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(1, 3));
+		
+		JButton actionButton_add = new JButton("추가");
+		actionButton_add.addActionListener(this);//
+		buttonPanel.add(actionButton_add);
+		
+		JButton actionButton_del = new JButton("제거");
+		actionButton_del.addActionListener(this);//
+		buttonPanel.add(actionButton_del);
+		
+		JButton actionButton_re = new JButton("변경사항저장");
+		actionButton_re.addActionListener(this);//
+		buttonPanel.add(actionButton_re);
+		
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		JPanel textPanel = new JPanel();
+		textPanel.setLayout(new FlowLayout());
+
+		JLabel nameps = new JLabel("이름 : ");
+		textPanel.add(nameps);
+		
+		name = new JTextField(10);
+		textPanel.add(name);
+		
+		JLabel subjps = new JLabel("과목 : ");
+		textPanel.add(subjps);
+		
+		subj = new JTextField(10);
+		textPanel.add(subj);
+		
+		JLabel gradeps = new JLabel("성적 : ");
+		textPanel.add(gradeps);
+		
+		grade = new JTextField(10);
+		textPanel.add(grade);
+		
+		
+		
+		add(textPanel, BorderLayout.CENTER);
 	}
 	//화면구성하기
 	
@@ -40,11 +93,18 @@ public class StudentGrade extends JFrame implements ActionListener{
 		// TODO Auto-generated method stub
 		String buttonString = e.getActionCommand();
 		
-		if (buttonString.contentEquals("")) {
+		if (buttonString.contentEquals("변경사항저장")) {
+			
+			System.exit(0);
+		}
+		else if (buttonString.contentEquals("추가")) {
 			
 			
 		}
-		
+		else if (buttonString.contentEquals("제거")) {
+			
+			
+		}
 		
 	}
 }
